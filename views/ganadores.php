abb9fd0fdc54bd303d3950201c9f1e667f1c40e9
Cesar Jr.Fontalvo <juniorfontalvo5@gmail.com>
2021-08-15 09:15:42 -0500

    Agregando las ultimas img y estilos

--- views/ganadores.php
+++ views/ganadores.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/estiloss.css">
+    <link rel="stylesheet" href="../css/estilos.css">
     <title>Carrera</title>
 
     <?php
@@ -23,8 +23,8 @@
     ?>
 
 </head>
-
-<body>
+<br>
+<body class="body-ganador">
     <div class ="container">
     <h1>Podio de la carrera</h1>
     <table class="table table-info">
@@ -68,13 +68,13 @@
             <button class="btn btn-success"><h4>Volver al inicio</h4></button>
 
         </form>
-
     </div>
+   </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     
-</div>
+   
 </body>
 
 </html>
